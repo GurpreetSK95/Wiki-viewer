f913057fcddd2e2c0f4d9ef026f51f6ce4190014
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-01-21 23:08:54 +0530

    feat: json callback implementation

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@
  */
 
 $(document).ready(function () {
-
+    var toSearch;
+    $("#search").bind("enterKey", function (data) {
+        toSearch = data;
+    });
+    var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var callback = '&callback=JSON_CALLBACK';
+    $.getJSON(api + toSearch + callback, function (json) {
+        console.log(api + toSearch);
+        $.each(json.query.pages, function (key, value) {
+            console.log(key + " " + value);
+        });
+    });
 });
